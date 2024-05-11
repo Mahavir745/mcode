3e9d674f8251a3c5e594f7179ae9a3ab0e1a710a
Mahavir <151805702+Mahavir745@users.noreply.github.com>
2024-05-11 11:58:31 +0530

    Update projectScript.js

--- projectScript.js
+++ projectScript.js
@@ -1,33 +1,21 @@
 const body = document.querySelector("#body-parts");
 const button = document.querySelector("#day-night");
-const imgEffect=document.querySelectorAll("#social-site li")
 const coloreffect=document.querySelector("#day-night")
-const footersection=document.querySelector("#about-section p");
-button.innerHTML="☀️";
-let light =button.style.backgroundColor="white";
+button.innerHTML="☀️"
 
+let light=button.style.backgroundColor="white";
 
-
-button.addEventListener("click", function() {
- 
+button.addEventListener("click", function(e) {
   if(light == 'white'){
-  body.style.backgroundColor = 'black'; 
-  body.style.color='white';
-  const svg=document.querySelector(".custom-shape-divider-bottom-1712813924 .shape-fill");
-  svg.style.fill='black';
-  button.innerHTML="🌑";
-
-  light=button.style.backgroundColor="black";
- 
-  imgEffect.forEach(function(images){
-    images.style.backgroundColor='white';
-    images.style.borderRadius="50%";
-  })
-  footersection.style.color='white';
-
-  }else{
-
-  footersection.style.color='black';
+    body.style.backgroundColor = 'black'; 
+    body.style.backgroundColor = 'black'; 
+    body.style.color='white';
+    const svg=document.querySelector(".custom-shape-divider-bottom-1712813924 .shape-fill");
+    svg.style.fill='black';
+    button.innerHTML="🌑";
+    light=button.style.backgroundColor="black";
+  }
+  else{
   body.style.backgroundColor = 'white'; 
   const svg=document.querySelector(".custom-shape-divider-bottom-1712813924 .shape-fill");
   svg.style.fill='white';
@@ -35,5 +23,7 @@ button.addEventListener("click", function() {
   light=button.style.backgroundColor="white";
 
   }
+
+
 });
 
